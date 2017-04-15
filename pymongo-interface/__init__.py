5db182c80e1f415b2a2c5018a8761d4c56f65119
whitetrees33 <huashli@mit.edu>
2017-04-15 12:29:05 -0400

    Add files via upload

---
+++ pymongo-interface/__init__.py
@@ -0,0 +1,12 @@
+"""
+Tata MongoDB manipulation module with Python.
+"""
+
+import sys
+import pymongo
+import pprint
+
+__all__ = ['backup_restore',
+           'control_account',
+           'modify_database']
+
